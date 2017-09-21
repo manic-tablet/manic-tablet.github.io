7f0354ac25ae67a23d201ccb7fcd14fb2590f180
Iñaki García Blasco <inaki@txorua.com>
2017-09-21 18:36:14 +0200

    Oculto menú una vez hago click en el item

--- assets/custom.js
+++ assets/custom.js
@@ -2,11 +2,15 @@ $(document).ready(function () {
   $('#main-nav .nav-item a').on('click', function (e) {
     //alert (this.hash)
 
+    $('#main-nav .navbar-toggler').removeClass('collapsed')
+    $('#navbarSupportedContent').removeClass('show')
+
     var target = this.hash
     var $target = $(target)
 
     $('html, body').stop().animate({
       'scrollTop': $target.offset().top - $('#main-nav').outerHeight()
     }, 900)
+
   })
 })
